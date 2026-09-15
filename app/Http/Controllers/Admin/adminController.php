4a0978dc3097ad2eecc4990a050fcbd382641308
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-15 15:28:42 +0330

    fix:change some methodes feat: create other methodes

--- app/Http/Controllers/Admin/adminController.php
+++ app/Http/Controllers/Admin/adminController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class adminController extends Controller
 {
     public function index(){
-        return view('admin.admin');
+        $users=User::query()->get();
+        return view('admin.users.showUsers', compact('users'));
     }
     public function create(Request $request)
     {
-        return view('admin.createUser');
+        return view('admin.users.createUser');
     }
 
     public function saveUser(Request $request)
@@ -23,27 +25,59 @@ class adminController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
-        return redirect('admin');
+        return redirect()->route('adminHome');
     }
 
     public function deleted_users()
     {
-
+        $deleted_users=User::query()->onlyTrashed()->get();
+        return view('admin.users.deleted_users', compact('deleted_users'));
     }
 
-    public function update(string $id)
+    public function update(Request $request, string $id)
     {
+        $user = User::query()->findOrFail($id);
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+        ]);
+        if ($request->filled('password')) {
+            $data['password'] =  Hash::make($request->password);
+        }
 
+        $user->update($data);
+        return redirect()->route('adminHome');
     }
 
     public function edit(string $id)
     {
-
+        $user = User::query()->find($id);
+        return view('admin.users.edit', compact('user'));
     }
 
     public function softDelete(string $id)
     {
+        $user = User::query()->find($id);
+        $user->delete();
+        return redirect()->route('admin.users.deleted_users');
+    }
 
+    public function restore(string $id)
+    {
+        $user = User::query()->onlyTrashed()->find($id);
+        $user->restore();
+
+        $deleted_users=User::query()->onlyTrashed()->get();
+        return view('admin.users.deleted_users', compact('deleted_users'));
+    }
+
+    public function hard_delete(string $id)
+    {
+        $user = User::query()->onlyTrashed()->find($id);
+        $user->forceDelete();
+
+        $deleted_users=User::query()->onlyTrashed()->get();
+        return view('admin.users.deleted_users', compact('deleted_users'));
     }
 
 }
